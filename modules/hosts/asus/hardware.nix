f06f19d158a0538de3a63f87f3be204e3c2bfa55
Simeon <111970903+simeonnv@users.noreply.github.com>
2026-02-22 14:21:41 +0200

    ez

--- modules/hosts/asus/hardware.nix
+++ modules/hosts/asus/hardware.nix
@@ -4,13 +4,34 @@
   ...
 }: {
   flake.nixosConfigurations.asus = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
     modules = with self.nixosModules; [
       asus
     ];
   };
 
-  flake.nixosModules.asus = {
+  flake.nixosModules.asus = {pkgs, ...}: {
+    system.stateVersion = "25.05";
     imports = [./_hardware-configuration.nix];
+    nixpkgs.config.allowUnfree = true;
+
     boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
+    boot.loader.systemd-boot.enable = false;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "asus";
+    networking.networkmanager.enable = true;
+    time.timeZone = "Europe/Sofia";
+
+    environment.systemPackages = with pkgs; [firefox git helix kitty];
+    users.users.simeon = {
+      isNormalUser = true;
+      description = "simeon";
+      extraGroups = ["networkmanager" "wheel"];
+    };
+
+    programs.firefox.enable = true;
   };
 }
