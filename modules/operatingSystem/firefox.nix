a1eb585e21329e94576a70fefde4d83498c55efa
simeon <simmeon.nv@proton.me>
2026-09-11 01:08:23 +0300

    firefox fix

--- modules/operatingSystem/firefox.nix
+++ modules/operatingSystem/firefox.nix
@@ -6,15 +6,6 @@
   flake.homeModules.firefox = {pkgs, ...}: let
     system = pkgs.stdenv.hostPlatform.system;
     addons = inputs.firefox-addons.packages.${system};
-
-    microslop = inputs.firefox-addons.lib.${system}.buildFirefoxXpiAddon {
-      pname = "microslop";
-      version = "0.0.5";
-      addonId = "microslop@4o4";
-      url = "https://addons.mozilla.org/firefox/downloads/file/4674546/microslop-0.0.5.xpi";
-      sha256 = "sha256-Et6yL9LyqUWpUKzi4t5x56vihf/z+EXN5EVjXeU1Qdk=";
-      meta = {};
-    };
   in {
     programs.firefox = {
       enable = true;
@@ -27,7 +18,6 @@
           packages = with addons; [
             foxyproxy-standard
             istilldontcareaboutcookies
-            microslop
             return-youtube-dislikes
             ublock-origin
             youtube-shorts-block
