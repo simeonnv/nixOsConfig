087b97bc88317c71a6dbc822b020806bf3ad84ce
simeon <simmeon.nv@proton.me>
2026-09-11 22:50:43 +0300

    gateway + ssh key

---
+++ modules/hosts/gateway/gateway.nix
@@ -0,0 +1,87 @@
+{
+  inputs,
+  self,
+  ownerProfile,
+  ...
+}: {
+  flake.nixosConfigurations.ppmg = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = with self.nixosModules;
+      [
+        pkgs-stable
+        ppmg
+        git
+        fastfetch
+        zsh
+        docker
+        ssh
+        kitty
+        sops
+        rust
+        screen
+        jujutsu
+        ai-slop
+      ]
+      ++ [
+        inputs.home-manager.nixosModules.home-manager
+        inputs.disko.nixosModules.disko
+        ./_disko.nix
+      ];
+  };
+
+  flake.nixosModules.ppmg = {pkgs, ...}: {
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+    hardware.enableAllFirmware = true;
+    networking.firewall.enable = true;
+
+    system.stateVersion = "25.11";
+    home-manager.backupFileExtension = "backup";
+    home-manager.users.${ownerProfile.name} = {pkgs, ...}: {
+      home.username = ownerProfile.name;
+      home.homeDirectory = "/home/${ownerProfile.name}";
+      home.stateVersion = "25.11";
+      imports = with self.homeModules; [
+        helix
+        fastfetch
+        git
+        yazi
+        btop
+        zsh
+        eza
+        zellij
+        direnv
+      ];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    # boot.kernelParams = ["consoleblank=60"];
+
+    services.logind.settings = {
+      Login = {
+        LidSwitchIgnoreInhibit = "no";
+        HandleLidSwitch = "ignore";
+        HandleLidSwitchExternalPower = "ignore";
+        HandleLidSwitchDocked = "ignore";
+      };
+    };
+    powerManagement.enable = true;
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
+    networking.hostName = "gateway";
+    networking.networkmanager.enable = true;
+
+    time.timeZone = "Europe/Sofia";
+
+    users.users.${ownerProfile.name} = {
+      isNormalUser = true;
+      description = ownerProfile.name;
+      extraGroups = ["networkmanager" "wheel"];
+    };
+  };
+}
--- modules/operatingSystem/ssh.nix
+++ modules/operatingSystem/ssh.nix
@@ -2,11 +2,19 @@
   pkgs,
   ownerProfile,
   ...
-}: {
+}: let
+  authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAz6cbpujcaBo5eAb4+c30kaD5T9wUXAtv5XS98BNACI simmeon.nv@proton.me"
+  ];
+in {
   flake.nixosModules.ssh = {pkgs, ...}: {
     networking.firewall.allowedTCPPorts = [22];
     services.openssh = {
       enable = true;
+      settings.PermitRootLogin = "prohibit-password";
     };
+
+    users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
+    users.users.${ownerProfile.name}.openssh.authorizedKeys.keys = authorizedKeys;
   };
 }
