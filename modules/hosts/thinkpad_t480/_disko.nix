bae9acdf220046ef6e74f29fcd515f6c906e67cb
simeon <simmeon.nv@proton.me>
2026-09-11 20:42:05 +0300

    Squash merge encryption_test into main

--- modules/hosts/thinkpad_t480/_disko.nix
+++ modules/hosts/thinkpad_t480/_disko.nix
@@ -23,17 +23,41 @@
             root = {
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = ["-f"];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                  };
-                  "/home" = {
-                    mountpoint = "/home";
-                  };
-                  "/nix" = {
-                    mountpoint = "/nix";
+                type = "luks";
+                name = "cryptroot";
+                settings = {
+                  allowDiscards = true;
+                  bypassWorkqueues = true;
+                };
+                extraFormatArgs = [
+                  "--type"
+                  "luks2"
+                  "--cipher"
+                  "aes-xts-plain64"
+                  "--key-size"
+                  "512"
+                  "--hash"
+                  "sha512"
+                  "--pbkdf"
+                  "argon2id"
+                  "--pbkdf-memory"
+                  "1048576"
+                  "--iter-time"
+                  "4000"
+                ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                    };
                   };
                 };
               };
